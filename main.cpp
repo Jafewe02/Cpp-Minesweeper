46f09fdb1169cd59b847a491a8feadff56882c66
Jafewe <mwelle231@gmail.com>
2025-11-13 21:50:30 +0100

    remove boilerplate code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <QApplication>
-#include "mainwindow.h"
+#include <QMainWindow>
+#include <QPushButton>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv);
 
-    MainWindow w;
-    w.show();
+  QMainWindow w;
+  w.setWindowTitle("Simple Main Window");
 
-    return a.exec();
+  w.setFixedSize(800, 800);
+
+  QPushButton *button = new QPushButton("Click Me", &w);
+  button->setText("Test Text");
+  button->setToolTip("This is a tooltip for a button");
+
+  w.show();
+  return a.exec();
 }
